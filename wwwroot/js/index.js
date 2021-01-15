324b3aa58773fed6b42c6ebba759ba8f0f43b309
Zachary Sorenson <zacharyjsorenson@gmail.com>
2021-01-15 11:17:46 -0500

    updated css and added info

---
+++ wwwroot/js/index.js
@@ -0,0 +1,10 @@
+$(document).ready( function(){
+
+    $('.nav-button').hover(
+        function(){
+            $(this).addClass('active');
+        }, function(){
+            $(this).removeClass('active');
+        }
+    )
+})
